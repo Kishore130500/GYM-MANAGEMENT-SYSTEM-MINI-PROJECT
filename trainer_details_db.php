91ae57d53b24fae65b122e09763b70ec094c5308
Kishore Chandran <kishorechandran66@gmail.com>
2022-03-10 20:32:59 +0530

    m

--- trainer_details_db.php
+++ trainer_details_db.php
@@ -19,7 +19,7 @@ if(!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !==true)
     <link rel="stylesheet" href="style.css">
     <!-- CSS only -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Document</title>
+    <title>DocG8 Fitness Centreument</title>
 </head>
 <body>
 
